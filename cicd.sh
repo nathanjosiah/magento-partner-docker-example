3314b44a9d2b843b691eb7cff93f5f33772eaf3e
Nathan Smith <nathsmit@adobe.com>
2020-06-08 10:23:56 -0500

    Switched to DinD

--- cicd.sh
+++ cicd.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
-echo 'Building Tool'
+info () {
+  echo -e "\033[0;34m" $* "\033[0m"
+}
+
+DIND_CONTAINER=$(docker run -d --rm --privileged -p 12375:2375 -p 8877:80 -e DOCKER_TLS_CERTDIR="" docker:dind)
+export DOCKER_HOST=tcp://localhost:12375
+
+info 'Building Tool'
 docker build -t tool ./tool
-echo 'Building PHP'
+info 'Building PHP'
 docker build -t myphp ./php
-echo 'Building magento'
+info 'Building magento'
 docker build -t magento ./magento
 
-if [[ ! -d magento-volume ]]; then
-  echo 'Creating volume directory'
-  mkdir magento-volume
-fi
-
-echo 'Maybe create volume'
+info 'Create volume'
 docker volume create \
-  --name mage \
-  --opt type=none \
-  --opt device=$(pwd)/magento-volume \
-  --opt o=bind
+  --name mage
 
+info 'Running Magento'
+MAGENTO_CONTAINER=$(docker run --rm -d \
+  -p 80:80 \
+  --name magento \
+  -v mage:/usr/share/nginx/html magento
+)
 
-
-echo 'Running Magento'
-MAGENTO_CONTAINER=$(docker run --rm -d -p 8765:80 -v mage:/usr/share/nginx/html magento)
-trap "docker stop $MAGENTO_CONTAINER" EXIT
-
-echo 'Running Tool'
+info 'Running Tool'
 docker run --rm \
+  --name tool \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v mage:/magento \
   tool \
   somearg
 
-echo 'Accessing "configured magento" loaded from github'
-curl http://localhost:8765/magento-ce/
+info 'Accessing "configured magento" loaded from github'
+echo -en "\033[0;33m"\
+  && curl http://localhost:8877/magento-ce/ \
+  && echo -en "\033[0m"
--- tool/script.sh
+++ tool/script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-docker run --rm myphp -r "echo 'Hello from docker myphp, you said $1';"
-echo
+echo -en "\033[0;33m"\
+  && docker run --rm myphp -r "echo 'Hello from docker myphp, you said $1';" \
+  && echo -e "\033[0m"
+
+echo -e "\033[0;34m" Running composer install "\033[0m"
 cp /composer.json /composer.lock /magento
 docker run --rm -v mage:/magento myphp /composer.phar install -d /magento
 docker run --rm -v mage:/magento alpine/git clone https://github.com/nathanjosiah/html-sample.git /magento/magento-ce
