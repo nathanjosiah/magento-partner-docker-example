640ed4b2fcb1c7fc211b5443dd4740cdcd89eee0
Nathan Smith <nathsmit@adobe.com>
2020-06-08 23:08:27 -0500

    Switched to magento cloud docker images. Actually installing magento

--- cicd.sh
+++ cicd.sh
@@ -9,31 +9,60 @@ export DOCKER_HOST=tcp://localhost:12375
 
 info 'Building Tool'
 docker build -t tool ./tool
-info 'Building PHP'
-docker build -t myphp ./php
-info 'Building magento'
-docker build -t magento ./magento
+info 'Building PHP CLI'
+docker build -t php-cli ./php-cli
+info 'Building PHP FPM'
+docker build -t mage-php-fpm ./php-fpm
+info 'Building Nginx'
+docker build -t mage-nginx ./nginx
+info 'Building Mariadb'
+docker build -t db ./db
 
 info 'Create volume'
-docker volume create \
-  --name mage
+docker volume create --name mage
 
-info 'Running Magento'
-MAGENTO_CONTAINER=$(docker run --rm -d \
-  -p 80:80 \
-  --name magento \
-  -v mage:/usr/share/nginx/html magento
-)
+info 'Create network'
+docker network create --driver bridge cicd
+
+info 'Running php-fpm'
+PHP_CONTAINER=$(docker run --rm -d \
+  -p 9000:9000 \
+  --name fpm \
+  --network cicd \
+  -v mage:/themount\
+  mage-php-fpm)
+docker exec $PHP_CONTAINER ls -s /themount/magento-ce /var/www/html
+
+info 'Running mariadb'
+docker run --rm -d \
+  -p 3306:3306 \
+  --name db \
+  --network cicd \
+  -e MYSQL_ROOT_PASSWORD=secretpw \
+  -e 'MYSQL_ROOT_HOST=%' \
+  -e MYSQL_DATABASE=main \
+  db
 
 info 'Running Tool'
 docker run --rm \
   --name tool \
+  --network cicd \
+  --env-file .env \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v mage:/magento \
   tool \
   somearg
 
-info 'Accessing "configured magento" loaded from github'
+info 'Running Nginx'
+MAGENTO_CONTAINER=$(docker run --rm -d \
+  --network cicd \
+  --name mage-nginx \
+  -v mage:/themount mage-nginx
+)
+docker exec $MAGENTO_CONTAINER mkdir /app
+docker exec $MAGENTO_CONTAINER ls -s /themount/magento-ce /app
+
+info 'Accessing magento'
 echo -en "\033[0;33m"\
-  && curl http://localhost:8877/magento-ce/ \
+  && docker run --network cicd --rm curlimages/curl http://mage-nginx/magento_version/ \
   && echo -en "\033[0m"
--- tool/script.sh
+++ tool/script.sh
@@ -1,9 +1,50 @@
 #!/bin/bash
-echo -en "\033[0;33m"\
-  && docker run --rm myphp -r "echo 'Hello from docker myphp, you said $1';" \
-  && echo -e "\033[0m"
 
-echo -e "\033[0;34m" Running composer install "\033[0m"
-cp /composer.json /composer.lock /magento
-docker run --rm -v mage:/magento myphp /composer.phar install -d /magento
-docker run --rm -v mage:/magento alpine/git clone https://github.com/nathanjosiah/html-sample.git /magento/magento-ce
+if [[ ! -d /magento/magento-ce ]]; then
+  echo -e "\033[0;34m" Installing Magento 2.3.4 package. "\033[0m"
+  # docker run --rm -v mage:/magento alpine/git clone --depth 1 --branch  /magento/magento-ce
+  mkdir /magento/.composer
+  cat << COMPOSER > /magento/.composer/auth.json
+  {
+      "http-basic": {
+          "repo.magento.com": {
+              "username": "${COMPOSER_USERNAME}",
+              "password": "${COMPOSER_PASSWORD}"
+          }
+      }
+  }
+COMPOSER
+
+  docker run --rm \
+    -v mage:/magento \
+    -e COMPOSER_HOME=/magento/.composer \
+    magento/magento-cloud-docker-php:7.2-cli-1.2 \
+    composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition:2.3.4 /magento/magento-ce
+
+  echo -e "\033[0;34m" Running highly unsafe permision change to 777 for everything for now. "\033[0m"
+  chmod -R 777 /magento/magento-ce
+
+  echo -e "\033[0;34m" Installing Magento "\033[0m"
+  docker run --rm \
+    --network cicd\
+    -v mage:/magento magento/magento-cloud-docker-php:7.2-cli-1.2\
+    php /magento/magento-ce/bin/magento setup:install \
+    --admin-firstname=Nathan \
+    --admin-lastname=Smith \
+    --admin-email=nathsmit@adobe.com \
+    --admin-user=admin \
+    --admin-password=123123q \
+    --base-url=http://magento/ \
+    --db-host=db \
+    --db-name=main \
+    --db-user=root \
+    --db-password=secretpw \
+    --currency=USD \
+    --timezone=America/Chicago \
+    --language=en_US \
+    --use-rewrites=1 \
+    --backend-frontname='admin'
+
+  docker run --rm --network cicd -v mage:/magento magento/magento-cloud-docker-php:7.2-cli-1.2\
+    php /magento/magento-ce/bin/magento de:mo:set production
+fi
