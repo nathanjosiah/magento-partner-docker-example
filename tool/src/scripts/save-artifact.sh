498d881ae4a8ae6eb942ac1af23ad1f6521bf5b1
Nathan Smith <nathsmit@adobe.com>
2020-07-07 16:34:57 -0500

    #33 Define output format for upgrade test

--- tool/src/scripts/save-artifact.sh
+++ tool/src/scripts/save-artifact.sh
@@ -3,9 +3,9 @@ path=$1
 name=$2
 purge=$3
 
-mkdir -p $ARTIFACT_DIRECTORY/$name
-cp -R $path/* $ARTIFACT_DIRECTORY/$name
-echo '[script] - Saving artifact directory from source' $path
+echo '[script] - Saving artifact directory from source' $path to $MAGE_ARTIFACTS_DIRECTORY/$name
+mkdir -p $MAGE_ARTIFACTS_DIRECTORY/$name
+cp -R $path/* $MAGE_ARTIFACTS_DIRECTORY/$name
 
 if [[ -n "$purge" ]]; then
   echo '[script] - Purging source artifact directory contents' $path
